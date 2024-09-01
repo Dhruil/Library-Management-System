adf095b9889ad6527c7ba7fd4330b244a6b45922
dhruil <dpldce2021@gmail.com>
2024-09-01 11:27:13 +0530

    wrote failling test cases for Creatbook

--- library.test.js
+++ library.test.js
@@ -1,25 +1,25 @@
 const createBook = require('./library')
 test("should create a book correctly", () => {
     const book = createBook(
-      "1234567890",
-      "Test Book",
-      "Test Author",
-      2023,
+      "9781612680194",
+      "Rich Dad Poor Dad",
+      "Robert Kiyosaki",
+      1997,
       5
     );
-    expect(book.isbn).toBe("1234567890");
-    expect(book.title).toBe("Test Book");
-    expect(book.author).toBe("Test Author");
-    expect(book.publicationYear).toBe(2023);
+    expect(book.isbn).toBe("9781612680194");
+    expect(book.title).toBe("Rich Dad Poor Dad");
+    expect(book.author).toBe("Robert Kiyosaki");
+    expect(book.publicationYear).toBe(1997);
     expect(book.stock).toBe(5);
   });
   test("should throw an error when creating a book without ISBN", () => {
     expect(() =>
-      createBook(null, "Test Book", "Test Author", 2023, 5)
+      createBook(null, "Rich Dad Poor Dad", "Robert Kiyosaki", 1997, 5)
     ).toThrow();
   });
 
   test("should set default stock to 1 when not provided", () => {
-    const book = createBook("1234567890", "Test Book", "Test Author", 2023);
+    const book = createBook("9781612680194", "Rich Dad Poor Dad", "Robert Kiyosaki", 1997);
     expect(book.stock).toBe(1);
   });
