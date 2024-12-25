67ba62807d1f928fefcfe1df4f506aa3d66da898
dhruil parmar <parmardhruil2611@gmail.com>
2024-12-25 22:54:43 +0530

    Modified The createBook Function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function createBook(isbn, title, author, publicationYear, stock) {
-    if (isbn == null) {
-        throw new Error("ISBN is required");
-      }
-      if (stock == null) {
-        stock = 1;
-      }else if (stock <= 0) {
+    if (
+      isbn == null ||
+      title == null ||
+      author == null ||
+      publicationYear == null ||
+      stock == null
+    ) {
+      throw new Error("Some Book Details Are Missing");
+    }else if (stock <= 0) {
         throw new Error("Stock must be greater than 0");
       }
     return {
